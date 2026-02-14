3faa30a9041c53ac9477ba657be02cfeb953967f
mmira100 <mmira100@yahoo.com>
2026-02-13 21:46:14 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from fastapi import FastAPI , Request
 import requests
 import json
 import  os
-
+from typing import Annotated
 app= FastAPI()
+
+#Abrir el archivo de configuración
+from pathlib import Path
+ruta_base = Path(__file__).parent 
+#archivo_ruta = ruta_base.parent / "config.json"
+archivo_ruta = ruta_base / "config.json"
+with open(archivo_ruta, 'r') as f:
+    data = json.load(f) # Lee y convierte a diccionario en un solo paso
+    url_token_wms = data["items"][0]["url_token_wms"]
 
 @app.get("/")
 def root():
-    return{"hello":"world3"}
+    return{"hello":url_token_wms }
 
 if __name__ == "__main__":
    import uvicorn
