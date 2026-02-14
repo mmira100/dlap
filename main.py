fd353432a846ae530f33e3add4afe05e06ad9c5c
mmira100 <mmira100@yahoo.com>
2026-02-14 12:35:48 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -18,11 +18,30 @@ archivo_ruta = ruta_base / "config.json"
 with open(archivo_ruta, 'r') as f:
     data = json.load(f) # Lee y convierte a diccionario en un solo paso
     url_token_wms = data["items"][0]["url_token_wms"]
+    usr_id_wms    = data["items"][0]["usr_id_wms"]
+    password_wms  = data["items"][0]["password_wms"]
+    url_wms_item  = data["items"][0]["url_wms_item"]
 
+# La API que queremos consumir para el token
+url = url_token_wms
+# The token parameters required by the API
+raw_data = f'{{"usr_id": "{usr_id_wms}", "password": "{password_wms}"}}'
+
+headers = {"Content-Type": "application/json"}
 @app.get("/")
 
 def root():
-   return{ "url_token_wms0" : url_token_wms }
+    # 1. Consumir la API externa usando requests
+    response = requests.post(url, data=raw_data, headers=headers)
+    #print(response.status_code)
+    # 2. Verificar que la respuesta es correcta
+    if response.status_code == 200:
+        for cookie in response.cookies:
+           if cookie.name == "MOCA-WS-SESSIONKEY":
+               v_token =cookie.value
+    
+    return{ "url_token_wms1" : url_token_wms,
+            "v_token1"        : v_token}
                        
 if __name__ == "__main__":
    import uvicorn
